c9529fb7f6609dbb1052173de804da21c58b2741
embyr333 <emb333@gmail.com>
2026-06-15 22:03:51 +0100

    Snapshot5: encompassing function, identified a bug

--- divvier3.py
+++ divvier3.py
@@ -9,55 +9,56 @@ a deterministic split for the 'Divvier' problem, to replace
 the original tool which used random sampling to process
 larger arrays, so could not guarentee an optimal split.
 
-Snapshot4: Minor changes.
-TODO: Tidy up, make an 'array -> best split function', GUI.
+Snapshot5: made encompassing function, identified a bug.
+TODO next: fix bug
+TODO later: Tidy up further, make a GUI.
 '''
-# def subcolls_gen(nums: list[float]) -> list[list[float]]: 
-# (Changed input element type hint to float to indicate  
-# elements do not have to be ints)
-def subcolls_gen(nums: list[float]) -> list[list[float]]: 
+
+def divvy(nums: list[float]) -> tuple[list[float], list[float]]: 
+
     subcolls: list[list[float]] = [[]]
     for num in nums:
         subcolls += [subcoll + [num] for subcoll in subcolls]
-    return subcolls
+    # Discard first (empty) item and complement (full input); (optional)
+    subcolls = subcolls[1 : len(subcolls) - 1]
+    print('subcolls', subcolls) # temp check
 
-input = [3,3,7,5,9]
+    subcoll_sums = []
+    for subcoll in subcolls:
+        subcoll_sums.append(sum(subcoll))
+    print('subcoll_sums', subcoll_sums) # temp check
 
-total = sum(input)
+    total = sum(nums)
+    half = total / 2
+    print('half', half) # temp check
 
-subcolls = subcolls_gen(input)
-# Discard first (empty) item and complement (full input); (optional)
-subcolls = subcolls[1 : len(subcolls) - 1]
-print('subcolls', subcolls) # temp check
+    min_offset = total # minimum offset from half (initialise at anything larger than half)
+    ioffm = 0 # index of (first found) minimum offset (from half); Note1
+    for i, subcoll_sum in enumerate(subcoll_sums):
+        if abs(subcoll_sum - half) < min_offset:
+            min_offset = abs(subcoll_sum - half)
+            ioffm = i
+    print('min_offset is', min_offset, 'for subcoll', subcolls[ioffm])
+    # min_offset is 0.5 for subcoll [3, 3, 7]
 
-subcoll_sums = []
-for subcoll in subcolls:
-    subcoll_sums.append(sum(subcoll))
-print('subcoll_sums', subcoll_sums) # temp check
+    # Now find a 'complement', i.e. a subset whose sum 
+    # with that of subcolls[ioffm] add to sum(input)
+    ioffc = 0 # index of (first found) complement
+    for i, subcoll_sum in enumerate(subcoll_sums):
+        if sum(subcolls[ioffm]) + subcoll_sum == total:
+            ioffc = i
+            break
+    print('complement is', subcolls[ioffc]) 
+    # complement is [5, 9]
 
-half = total / 2
-print('half', half) # temp check
-min_offset = total # minimum offset from half 
-# (initialise at anything larger than half)
-# ioffmo = 0 # index of (first found) minimum offset (from half)
-# ...typo (which does not interfere unless input has just single element)
-ioffm = 0 # index of (first found) minimum offset (from half)
-# ...can discard this when input length > 1 is enforced)
+    return subcolls[ioffm], subcolls[ioffc]
 
-for i, subcoll_sum in enumerate(subcoll_sums):
-    if abs(subcoll_sum - half) < min_offset:
-        min_offset = abs(subcoll_sum - half)
-        ioffm = i
+# Note1: Can discard this pre-declaration/initialisation 
+# outside loop when input length > 1 is enforced
 
-print('min_offset is', min_offset, 'for subcoll', subcolls[ioffm])
-# min_offset is 0.5 for subcoll [3, 3, 7]
 
-# Now find a 'complement', i.e. a subset whose sum 
-# with that of subcolls[ioffm] add to sum(input)
-ioffc = 0 # index of (first found) complement
-for i, subcoll_sum in enumerate(subcoll_sums):
-    if sum(subcolls[ioffm]) + subcoll_sum == total:
-        ioffc = i
-        break
-print('complement is', subcolls[ioffc]) 
-# complement is [5, 9]
+# Some tests
+print(divvy([3,3,7,5,9])) # ([3, 3, 7], [5, 9])
+print(divvy([1,2,3])) # ([1, 2], [1, 2]) --bug: need to exclude 
+# the first-found subcollection from search for its complement
+# as it will be its own complement if split is exact
