50bf897df69b18d854bc8e9364ed5a017612fc95
embyr333 <emb333@gmail.com>
2026-06-15 23:17:37 +0100

    Snapshot6: fixed bug; added a few more tests.

--- divvier3.py
+++ divvier3.py
@@ -9,9 +9,15 @@ a deterministic split for the 'Divvier' problem, to replace
 the original tool which used random sampling to process
 larger arrays, so could not guarentee an optimal split.
 
-Snapshot5: made encompassing function, identified a bug.
-TODO next: fix bug
-TODO later: Tidy up further, make a GUI.
+Snapshot6: --fixed bug; added a few more tests.
+
+TODO: 
+- More tests, checking with original Divvier.
+- Maybe change to returning a string as per original program reporting
+"Smallest difference found: ---, between subcollection ---, totalling ---
+and reciprocal subcollection ---, totalling ---",
+or immediately return a message re irrelevance if <2 elements.
+- Make GUI
 '''
 
 def divvy(nums: list[float]) -> tuple[list[float], list[float]]: 
@@ -21,16 +27,16 @@ def divvy(nums: list[float]) -> tuple[list[float], list[float]]:
         subcolls += [subcoll + [num] for subcoll in subcolls]
     # Discard first (empty) item and complement (full input); (optional)
     subcolls = subcolls[1 : len(subcolls) - 1]
-    print('subcolls', subcolls) # temp check
+    # print('subcolls', subcolls) # temp check
 
     subcoll_sums = []
     for subcoll in subcolls:
         subcoll_sums.append(sum(subcoll))
-    print('subcoll_sums', subcoll_sums) # temp check
+    # print('subcoll_sums', subcoll_sums) # temp check
 
     total = sum(nums)
     half = total / 2
-    print('half', half) # temp check
+    # print('half', half) # temp check
 
     min_offset = total # minimum offset from half (initialise at anything larger than half)
     ioffm = 0 # index of (first found) minimum offset (from half); Note1
@@ -38,18 +44,16 @@ def divvy(nums: list[float]) -> tuple[list[float], list[float]]:
         if abs(subcoll_sum - half) < min_offset:
             min_offset = abs(subcoll_sum - half)
             ioffm = i
-    print('min_offset is', min_offset, 'for subcoll', subcolls[ioffm])
-    # min_offset is 0.5 for subcoll [3, 3, 7]
+    # print('min_offset is', min_offset, 'for subcoll', subcolls[ioffm])
 
     # Now find a 'complement', i.e. a subset whose sum 
     # with that of subcolls[ioffm] add to sum(input)
     ioffc = 0 # index of (first found) complement
     for i, subcoll_sum in enumerate(subcoll_sums):
-        if sum(subcolls[ioffm]) + subcoll_sum == total:
+        if sum(subcolls[ioffm]) + subcoll_sum == total and i != ioffm: # --added condition to fix bug
             ioffc = i
             break
-    print('complement is', subcolls[ioffc]) 
-    # complement is [5, 9]
+    # print('complement is', subcolls[ioffc]) 
 
     return subcolls[ioffm], subcolls[ioffc]
 
@@ -59,6 +63,6 @@ def divvy(nums: list[float]) -> tuple[list[float], list[float]]:
 
 # Some tests
 print(divvy([3,3,7,5,9])) # ([3, 3, 7], [5, 9])
-print(divvy([1,2,3])) # ([1, 2], [1, 2]) --bug: need to exclude 
-# the first-found subcollection from search for its complement
-# as it will be its own complement if split is exact
+print(divvy([1,2,3])) # ([1, 2], [3])
+print(divvy([1,2])) # ([1], [2])
+print(divvy([1,2,1,2])) # ([1, 2], [2, 1])
