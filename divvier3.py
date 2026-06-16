6853751a4162327782f5689324ca5bf12532b2dd
embyr333 <emb333@gmail.com>
2026-06-16 14:54:24 +0100

    Snapshot8: Return message for single-element arg

--- divvier3.py
+++ divvier3.py
@@ -9,13 +9,7 @@ a deterministic split for the 'Divvier' problem, to replace
 the original tool which used random sampling to process
 larger arrays, so could not guarentee an optimal split.
 
-Snapshot7: Adding another test, which happened to have a subcollection
-that summed to half total, found another bug. This arises from 
-the flaw of making replicate subcollections other than any that 
-may legitimately be present due to replication of some elements.
-To avoid picking 'false complements', switched to just determining 
-remaining elements in input list by deleting from (a copy of) the 
-input list; this is of course time-inefficient.
+Snapshot8: Choosing to return a message if single-element list is passed in.
 
 TODO: 
 - More tests, checking with original Divvier.
@@ -27,11 +21,13 @@ or immediately return a message re irrelevance if <2 elements.
 '''
 
 def divvy(nums: list[float]) -> tuple[list[float], list[float]]: 
+    if len(nums) < 2: # Exception to return type hint...
+        return 'Single-element inputs are not relevant.'
 
     subcolls: list[list[float]] = [[]]
     for num in nums:
         subcolls += [subcoll + [num] for subcoll in subcolls]
-    # Discard first (empty) item and complement (full input); (optional)
+    # Discard first (empty) item and complement (full input); Note1
     subcolls = subcolls[1 : len(subcolls) - 1]
     # print('subcolls', subcolls) # temp check
 
@@ -45,36 +41,28 @@ def divvy(nums: list[float]) -> tuple[list[float], list[float]]:
     # print('total', total, '  half', half) # temp check
 
     min_offset = total # minimum offset from half (initialise at anything larger than half)
-    ioffm = 0 # index of (first found) minimum offset (from half); Note1
+    # ioffm = 0 # index of (first found) minimum offset (from half); Note2
     for i, subcoll_sum in enumerate(subcoll_sums):
         if abs(subcoll_sum - half) < min_offset:
             min_offset = abs(subcoll_sum - half)
             ioffm = i
     # print('min_offset is', min_offset, 'for subcoll', subcolls[ioffm])
 
-
-    # Now find a 'complement', i.e. a subset whose sum 
-    # with that of subcolls[ioffm] add to sum(input)
-    # ioffc = 0 # index of (first found) complement
-    # for i, subcoll_sum in enumerate(subcoll_sums):
-    #     if sum(subcolls[ioffm]) + subcoll_sum == total and i != ioffm: 
-    #         ioffc = i
-    #         break
-    # --change to afix bug described above
-    complement = nums.copy() # Note2
-    for e in subcolls[ioffm]: # Note3
+    complement = nums.copy() # Note3
+    for e in subcolls[ioffm]: # Note4
         complement.remove(e)
     # print('complement', complement) # temp check
 
-    # return subcolls[ioffm], subcolls[ioffc]
     return subcolls[ioffm], complement
 
 '''
-Note1: Can discard this pre-declaration/initialisation 
-# outside loop when input length > 1 is enforced.
-Note2: Making copy in case we need to avoid modifying the input list.
+Note1: This would need to be removed if single-element inputs 
+are possible, and the handling of these with an 'error message'
+were not present at the beginning of the function.
+Note2: ...Conversely, this would need to be put back.
+Note3: Making copy in case we need to avoid modifying the input list.
 Shallow copy is sufficient as elements are primitives.
-Note3: This is of course time-inefficient.
+Note4: This is of course time-inefficient.
 '''
 
 
@@ -83,9 +71,5 @@ print(divvy([3,3,7,5,9])) # ([3, 3, 7], [5, 9])
 print(divvy([1,2,3])) # ([1, 2], [3])
 print(divvy([1,2])) # ([1], [2])
 print(divvy([1,2,1,2])) # ([1, 2], [2, 1])
-
 print(divvy([78, 93, 44, 27, 58, 25, 27, 73, 55, 32]))
-# ([78, 93, 27, 58], [78, 93, 58, 27]) --exposes bug:
-# replicate subcolls that are not actually complements 
-# After fixing:
-# ([78, 93, 27, 58], [44, 25, 27, 73, 55, 32])
+print(divvy([1])) # Single-element inputs are not relevant.
