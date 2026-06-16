1d97a382efe6fe70a9d9269ccfb23f7c26d3b00f
embyr333 <emb333@gmail.com>
2026-06-16 14:26:49 +0100

    Snapshot7: New test exposed another bug; fixed

--- divvier3.py
+++ divvier3.py
@@ -9,7 +9,13 @@ a deterministic split for the 'Divvier' problem, to replace
 the original tool which used random sampling to process
 larger arrays, so could not guarentee an optimal split.
 
-Snapshot6: --fixed bug; added a few more tests.
+Snapshot7: Adding another test, which happened to have a subcollection
+that summed to half total, found another bug. This arises from 
+the flaw of making replicate subcollections other than any that 
+may legitimately be present due to replication of some elements.
+To avoid picking 'false complements', switched to just determining 
+remaining elements in input list by deleting from (a copy of) the 
+input list; this is of course time-inefficient.
 
 TODO: 
 - More tests, checking with original Divvier.
@@ -36,7 +42,7 @@ def divvy(nums: list[float]) -> tuple[list[float], list[float]]:
 
     total = sum(nums)
     half = total / 2
-    # print('half', half) # temp check
+    # print('total', total, '  half', half) # temp check
 
     min_offset = total # minimum offset from half (initialise at anything larger than half)
     ioffm = 0 # index of (first found) minimum offset (from half); Note1
@@ -46,19 +52,30 @@ def divvy(nums: list[float]) -> tuple[list[float], list[float]]:
             ioffm = i
     # print('min_offset is', min_offset, 'for subcoll', subcolls[ioffm])
 
+
     # Now find a 'complement', i.e. a subset whose sum 
     # with that of subcolls[ioffm] add to sum(input)
-    ioffc = 0 # index of (first found) complement
-    for i, subcoll_sum in enumerate(subcoll_sums):
-        if sum(subcolls[ioffm]) + subcoll_sum == total and i != ioffm: # --added condition to fix bug
-            ioffc = i
-            break
-    # print('complement is', subcolls[ioffc]) 
+    # ioffc = 0 # index of (first found) complement
+    # for i, subcoll_sum in enumerate(subcoll_sums):
+    #     if sum(subcolls[ioffm]) + subcoll_sum == total and i != ioffm: 
+    #         ioffc = i
+    #         break
+    # --change to afix bug described above
+    complement = nums.copy() # Note2
+    for e in subcolls[ioffm]: # Note3
+        complement.remove(e)
+    # print('complement', complement) # temp check
 
-    return subcolls[ioffm], subcolls[ioffc]
+    # return subcolls[ioffm], subcolls[ioffc]
+    return subcolls[ioffm], complement
 
-# Note1: Can discard this pre-declaration/initialisation 
-# outside loop when input length > 1 is enforced
+'''
+Note1: Can discard this pre-declaration/initialisation 
+# outside loop when input length > 1 is enforced.
+Note2: Making copy in case we need to avoid modifying the input list.
+Shallow copy is sufficient as elements are primitives.
+Note3: This is of course time-inefficient.
+'''
 
 
 # Some tests
@@ -66,3 +83,9 @@ print(divvy([3,3,7,5,9])) # ([3, 3, 7], [5, 9])
 print(divvy([1,2,3])) # ([1, 2], [3])
 print(divvy([1,2])) # ([1], [2])
 print(divvy([1,2,1,2])) # ([1, 2], [2, 1])
+
+print(divvy([78, 93, 44, 27, 58, 25, 27, 73, 55, 32]))
+# ([78, 93, 27, 58], [78, 93, 58, 27]) --exposes bug:
+# replicate subcolls that are not actually complements 
+# After fixing:
+# ([78, 93, 27, 58], [44, 25, 27, 73, 55, 32])
